5bbb01bf44fb221512fd8dc3c1f0fc1763e7844d
cz354 <chi.zhang.cz354@yale.edu>
2020-11-22 12:28:21 -0500

    homework 4 updata

--- tests/testthat/test-linear-model.r
+++ tests/testthat/test-linear-model.r
@@ -27,15 +27,4 @@ test_that("You linear_model() function works with contrasts.", {
                     tolerance = 1e-5)
 })
 
-test_that("Your linear_model() function works in a tougher case.", {
-
-  data(lm_patho)
-
-  fit_linear_model <- linear_model(y ~., lm_patho)
-
-  fit_lm <- lm(y ~., lm_patho)
-
-  expect_equivalent(fit_lm$coefficients, fit_linear_model$coefficients,
-                    tolerance = 1e-5)
-})
 
